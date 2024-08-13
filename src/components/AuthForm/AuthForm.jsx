bd08ce87e96e1368ac95968273623cdeae00a7df
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-08-13 07:39:02 +0100

    added input AuthForm

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,9 +1,25 @@
+import { Box, Image, Input, VStack } from '@chakra-ui/react';
 import React from 'react'
 
 export const AuthForm = () => {
-  return (
-    <div>AuthForm</div>
-  )
+  return <>
+    <Box border={"1px solid blue"} borderRadius={4} padding={5}>
+    <VStack spacing={4}>
+      <Image  src='/logo.png' h={22} cursor={"pointer"} alt='instagram'/>
+      <Input
+        placeholder='Email'
+        fontSize={14}
+        type='email'
+       />
+       <Input
+         placeholder='Password'
+         fontSize={14}
+         type='password'
+        /> 
+    </VStack>
+    </Box>
+  </>
+  
 }
 
 export default AuthForm;
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -6,18 +6,20 @@ export const AuthPage = () => {
   return (
     <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
         <Container maxW={"container.md"} padding={0}>
+           <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
             {/* left hand siz */ }
             <Box display={{ base: "none", md: "block"}}>
                 <Image src='/auth.png' h={450} alt='Phone img'/>
             </Box>
             <VStack spacing={4} align={"stretch"}>
             <AuthForm />
-            <Box>Get the app</Box>
+            <Box textAlign={"center"}>Get the app</Box>
             <Flex gap={5} justifyContent={"center"}>
             <Image  src='/playstore.png' h={"10"} alt='playstore logo' />
             <Image  src='/microsoft.png' h={"10"} alt='microsoft logo' />
             </Flex>
             </VStack>
+            </Flex>
         </Container>
     </Flex>
   )
